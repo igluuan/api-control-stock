640d09910e08c4b5a9cd3701333a4fbece7943a1
devluan <contatoluansantos7@gmail.com>
2025-02-21 14:13:35 -0300

    feat: create project

---
+++ src/main/java/tech/devluan/api_control_stock/ApiControlStockApplication.java
@@ -0,0 +1,13 @@
+package tech.devluan.api_control_stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiControlStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiControlStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/devluan/api_control_stock/ApiControlStockApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.devluan.api_control_stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiControlStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
